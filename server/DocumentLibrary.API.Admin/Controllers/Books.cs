ac88263cab1f5b87aa1226be0d1e45b0d2a83a87
Ivaylo Botusharov <ivaylo.botusharov@gmail.com>
2021-01-21 23:36:49 +0200

    added initial Solution and Admin API

---
+++ server/DocumentLibrary.API.Admin/Controllers/Books.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using DocumentLibrary.API.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentLibrary.API.Admin.Controllers
+{
+    [ApiController]
+    [Route("Admin/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        public BooksController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var books = new List<Book>
+            {
+                new Book
+                {
+                    Name = "Foundation"
+                },
+                new Book
+                {
+                    Name = "The Call of Ktulu"
+                }
+            };
+            
+            return Ok(books);
+        }
+    }
+}
---
+++ server/DocumentLibrary.API.Admin/Models/Book.cs
@@ -0,0 +1,9 @@
+namespace DocumentLibrary.API.Admin.Models
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+    }
+}
